2d4111842e1146cef3f7496264403b6507279d5a
isiahZaye <isiaharrufat@gmail.com>
2020-02-07 09:21:56 -0500

    javascript update

---
+++ js/index.js
@@ -0,0 +1,11 @@
+class Players{
+  constructor(){
+    this.odd = "odd",
+    this.even = "even"
+  }
+
+}
+const bot = ["odd", "even", "odd", "even"];
+
+let player1 = new Players()
+let player2 = new Players()
