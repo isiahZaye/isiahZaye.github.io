2d0b967013a595ecbdd50afe469958b49004f95d
isiahZaye <isiaharrufat@gmail.com>
2020-02-08 11:12:05 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Mod 1 project: The Game
+" odd or even " is the game i will be creating
+rules of the game
+
+ this is a game of odd and even where players must choose between one of the choices.
+Players have 3 seconds to pick their choice. Once three seconds are up, a bot will randomize an outcome. Whichever player matches the bots outcome will be determined the winner. But no worries if you lost, this game is a best of 5!
--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
+const bot = ["odd", "even", "odd", "even"];
+let time = 3;
+let gameCountdown = document.getElementById("gamecountdown")
+
 class Players{
   constructor(){
     this.odd = "odd",
     this.even = "even"
   }
-
-}
-const bot = ["odd", "even", "odd", "even"];
+};
 
 let player1 = new Players()
 let player2 = new Players()
+
+
+
+const play = () => {
+  while (time > 0) {
+
+    gameCountdown.innerHTML += `<div class="gamecountdown">${time}</div>`
+    time --
+
+  }
+
+}
+console.log(gamecountdown)
